4416fe1329081834e1ef79e46dcec94af13b3941
Jonasuto <jonasdataman@gmail.com>
2015-10-11 18:17:09 -0500

    ac

--- app/src/main/java/mx/itesm/jcampos/huntingcows/EscenaCazaJurasica.java
+++ app/src/main/java/mx/itesm/jcampos/huntingcows/EscenaCazaJurasica.java
@@ -6,39 +6,37 @@ import org.andengine.entity.scene.menu.item.SpriteMenuItem;
 import org.andengine.entity.scene.menu.item.decorator.ScaleMenuItemDecorator;
 import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.texture.region.TiledTextureRegion;
+
 
 /**
  * Created by Campos on 11/10/15.
  */
 public class EscenaCazaJurasica extends EscenaBase {
 
-    private ITextureRegion regionsiguiente;
-    private ITextureRegion regionanterior;
+    private ITextureRegion regionFlecha;
     private ITextureRegion regionFondo;
 
-    private AnimatedSprite spritePersonaje;
+
+
+    private Jugador spritePersonaje;
 
     private TiledTextureRegion regionPersonajeAnimado;
-
-    private final int OPCION_DERECHA = 0;
-    private final int OPCION_IZQUIERDA = 1;
-
-
-
-    private MenuScene menu;
 
 
     // Sprite para el fondo
     private Sprite spriteFondo;
 
+    private Sprite spriteFlechaIzquierda;
+    private Sprite spriteFlechaDerecha;
+
     @Override
     public void cargarRecursos() {
 
         regionFondo = cargarImagen("Imagenes/Niveles/fondo.jpg");
-        regionsiguiente = cargarImagen("Imagenes/Historia/flecha.png");
-        regionanterior = cargarImagen("Imagenes/Historia/flecha.png");
+        regionFlecha=cargarImagen("Imagenes/Historia/flecha.png");
 
         regionPersonajeAnimado = cargarImagenMosaico("Imagenes/kiki.png", 600, 158, 1, 4);
 
@@ -47,76 +45,24 @@ public class EscenaCazaJurasica extends EscenaBase {
 
     @Override
     public void crearEscena() {
+
         spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionFondo);
         attachChild(spriteFondo);
-        agregarMenu();
 
-        spritePersonaje = new AnimatedSprite(-ControlJuego.ANCHO_CAMARA/4+ControlJuego.ANCHO_CAMARA, ControlJuego.ALTO_CAMARA/4,regionPersonajeAnimado, actividadJuego.getVertexBufferObjectManager());
+        spritePersonaje = new Jugador(-ControlJuego.ANCHO_CAMARA/4+ControlJuego.ANCHO_CAMARA, ControlJuego.ALTO_CAMARA/4,regionPersonajeAnimado, actividadJuego.getVertexBufferObjectManager());
         spritePersonaje.animate(200);
         attachChild(spritePersonaje);
 
         admMusica.cargarMusica(2);
 
-    }
+        spriteFlechaIzquierda= spriteFondo = cargarSprite((ControlJuego.ANCHO_CAMARA/2)-130, ControlJuego.ALTO_CAMARA / 2, regionFlecha);
+        spriteFlechaIzquierda.setRotation(-180);
+        attachChild(spriteFlechaIzquierda);
 
-    private void agregarMenu() {
-        // Crea el objeto que representa el menú
-        menu = new MenuScene(actividadJuego.camara);
-        // Centrado en la pantalla
-        menu.setPosition(ControlJuego.ANCHO_CAMARA/2,ControlJuego.ALTO_CAMARA/2);
-
-        // Crea las opciones (por ahora, acerca de y jugar)
-        IMenuItem opcionSiguiente = new ScaleMenuItemDecorator(new SpriteMenuItem(OPCION_DERECHA,
-                regionsiguiente, actividadJuego.getVertexBufferObjectManager()), 1.5f, 1);
-
-        IMenuItem opcionanterior = new ScaleMenuItemDecorator(new SpriteMenuItem(OPCION_IZQUIERDA,
-                regionanterior, actividadJuego.getVertexBufferObjectManager()), 1.5f, 1);
+        spriteFlechaDerecha= spriteFondo = cargarSprite(ControlJuego.ANCHO_CAMARA / 2, ControlJuego.ALTO_CAMARA / 2, regionFlecha);
+        attachChild(spriteFlechaDerecha);
 
 
-        // Agrega las opciones al menú
-        menu.addMenuItem(opcionSiguiente);
-        menu.addMenuItem(opcionanterior);
-
-        // que es esto??
-
-
-        // Termina la configuración
-        menu.buildAnimations();
-        menu.setBackgroundEnabled(false);   // Completamente transparente
-
-        // Ubicar las opciones DENTRO del menú. El centro del menú es (0,0)
-        opcionSiguiente.setPosition((-ControlJuego.ANCHO_CAMARA/5)-110, -ControlJuego.ANCHO_CAMARA/4);
-
-        opcionanterior.setPosition((-ControlJuego.ANCHO_CAMARA/3)-160, -ControlJuego.ANCHO_CAMARA/4);
-        opcionanterior.setRotation(-180);
-
-
-        // Registra el Listener para atender las opciones
-        menu.setOnMenuItemClickListener(new MenuScene.IOnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
-                                             float pMenuItemLocalX, float pMenuItemLocalY) {
-                // El parámetro pMenuItem indica la opción oprimida
-                switch (pMenuItem.getID()) {
-
-                    case OPCION_DERECHA:
-                        admMusica.reproducirMusicaBoton();
-                        spritePersonaje.setX(spritePersonaje.getX()+50);
-                        break;
-
-                    case OPCION_IZQUIERDA:
-                        // Mostrar la escena de AcercaDe
-                        admMusica.reproducirMusicaBoton();
-                        spritePersonaje.setX(spritePersonaje.getX()-50);
-                        break;
-
-                }
-                return true;
-            }
-        });
-
-        // Asigna este menú a la escena
-        setChildScene(menu);
     }
 
     @Override
@@ -135,7 +81,10 @@ public class EscenaCazaJurasica extends EscenaBase {
 
     protected void onManagedUpdate(float pSecondsElapsed) {
         super.onManagedUpdate(pSecondsElapsed);
+
     }
+
+
 
     @Override
     public void liberarEscena() {
